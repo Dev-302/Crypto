6e7bc3d52226fe60166e2147b89033174b90f955
Dev-302 <gebruiker302@outlook.com>
2021-07-10 23:06:36 +0200

    contracts

---
+++ Election/contracts/Election.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.4.11;
+
+contract Election {
+    // Store candidate
+    // Read candidate
+    string public candidate;
+    // Constructor
+    function Constructor () public {
+        candidate = "Candidate 1";
+    }
+}
